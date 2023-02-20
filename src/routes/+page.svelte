4c628ba814e8a7039bf20ab6f59306a9acbcf19a
pattarakom-m <pattarakon.m@tripatschool.ac.th>
2023-02-20 16:04:47 +0700

    a bit modify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+	import { Styles , Button } from 'sveltestrap';
+</script>
+<Styles />
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="https://sveltestrap.js.org/?path=/story/components--get-started">https://sveltestrap.js.org/?path=/story/components--get-started</a>
+<br>
+<button>test</button>
